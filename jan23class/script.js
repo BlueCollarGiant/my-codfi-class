dac7b96e16c37a30f0987774afb437a1c6fe95c6
BlueCollarGiant <167778887+BlueCollarGiant@users.noreply.github.com>
2025-01-23 19:11:02 -0600

    finished class

---
+++ jan23class/script.js
@@ -0,0 +1,10 @@
+const element = document.getElementById('mydiv');
+console.log(`element:`, element);
+
+
+
+console.log(element.textContent);
+element.textContent = 'what the heck?';
+const myButton = document.querySelector('.myButton');
+
+console.log(myButton);
