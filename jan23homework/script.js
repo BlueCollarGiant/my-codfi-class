2b3b89d6e7121abe5999a3e0fd9811986bf0c2ed
BlueCollarGiant <167778887+BlueCollarGiant@users.noreply.github.com>
2025-01-23 20:12:52 -0600

    brain wont work taking rest

---
+++ jan23homework/script.js
@@ -0,0 +1,33 @@
+
+/*excercise one 
+const car = {
+    make: "vw",
+    model: "bettle",
+    year: 1987,
+
+
+    getInfo: function() {
+        console.log(`make: ${car.make} Model: ${car.model} Year: ${car.year}`)
+    }
+};
+
+car.getInfo();
+ end of one */
+ // start of three
+/* function changeBackgroundColor(){
+    const div= document.getElementById("mydiv");
+    div.style.backgroundColor = "lightblue";
+ }
+ changeBackgroundColor();*/
+ // start of 4
+ /*
+ const button = document.getElementById("myButton");
+ const text = document.getElementById("mytext");
+
+ button.addEventListener("click", function() {
+    text.textContent = "This text has been changed";
+ });*/
+ // end of 4
+ // start of 5
+ 
+
