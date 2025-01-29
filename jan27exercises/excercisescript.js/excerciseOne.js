22cfba537fe68aa334522f3798e25b0d91e44c2a
BluecollarGiant <Mr.isaiahratliff@gmail.com>
2025-01-29 17:51:14 -0600

    setup foundation for class exercises

---
+++ jan27exercises/excercisescript.js/excerciseOne.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    alert("welcome to my page!");
+};
+
+// learned about the window.onload had to look it up because i havnt ran into it before.
+
+// excercise one complete
