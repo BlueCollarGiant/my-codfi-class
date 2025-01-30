c2eb83414b5fc74111e731f5a85a92563e4379f2
BluecollarGiant <Mr.isaiahratliff@gmail.com>
2025-01-29 18:16:05 -0600

    fixed file name and exercise two

---
+++ jan27exercises/exercisescript.js/exerciseTwo.js
@@ -0,0 +1,12 @@
+/*window.onload = function() {
+    alert("welcome to my page!");
+};*/
+
+
+function changeText() {
+    const paragraph = document.getElementById("target-text");
+    paragraph.innerText = " You changed me"
+}
+
+// took me a while still having trouble with onclick just getting it into my head that its onclick = "function name " 
+// probably need to memorize that since ive heard angular uses onclick alot
