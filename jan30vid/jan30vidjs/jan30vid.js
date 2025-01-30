f278db02def1547c5cbc6b52e1e3604bac1dbc48
BluecollarGiant <Mr.isaiahratliff@gmail.com>
2025-01-30 12:50:08 -0600

    finished 3rd project

--- jan30vid/jan30vidjs/jan30vid.js
+++ jan30vid/jan30vidjs/jan30vid.js
@@ -1,29 +1,46 @@
-const todoList = ['make diner', 'wash dishes'];
+const todoList = [{
+    name:'make diner', 
+    dueDate: '01-30-2025'}, {
+    
+    name: 'wash dishes',
+    dueDate: '01-30-2025'}];
 
 renderTodoList();
+
 function renderTodoList() {
     let todoListHTML = "";
 
     for (let i = 0; i < todoList.length; i++){
-        const todo = todoList[i];
-        const html = `<p>${todo}</p>`;
+        const todoObject = todoList[i];
+        const { name, dueDate } = todoObject;
+        const html = 
+        `
+        <div>${name}</div> 
+        <div> ${dueDate}</div> 
+        <button onclick= "
+            todoList.splice(${i}, 1);
+            renderTodoList();
+        " class = "delete-todo-button">Delete</button>
+        `;
         todoListHTML += html;
     }
 
 
-    console.log(todoListHTML);
+    
 
     document.querySelector(".js-todo-list")
         .innerHTML = todoListHTML;
 }
 
 function addTodo() {
-    const inputElement = document.querySelector(".js-name-input")
+    const inputElement = document.querySelector(".js-name-input");
     const name = inputElement.value; 
+    const dateInputElement = document.querySelector(".js-due-date-input");
+    const dueDate = dateInputElement.value;
+    todoList.push({
+        name: name,
+        dueDate: dueDate });
     
-
-    todoList.push(name);
-    console.log(todoList);
 
     inputElement.value = "";
 
