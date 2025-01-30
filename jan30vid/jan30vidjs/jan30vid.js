c7d004585ce7863f08c2c284be166792fc0a2c1f
BluecollarGiant <Mr.isaiahratliff@gmail.com>
2025-01-30 11:53:48 -0600

    finishes second todo list project

--- jan30vid/jan30vidjs/jan30vid.js
+++ jan30vid/jan30vidjs/jan30vid.js
@@ -1,4 +1,21 @@
-const todoList = [];
+const todoList = ['make diner', 'wash dishes'];
+
+renderTodoList();
+function renderTodoList() {
+    let todoListHTML = "";
+
+    for (let i = 0; i < todoList.length; i++){
+        const todo = todoList[i];
+        const html = `<p>${todo}</p>`;
+        todoListHTML += html;
+    }
+
+
+    console.log(todoListHTML);
+
+    document.querySelector(".js-todo-list")
+        .innerHTML = todoListHTML;
+}
 
 function addTodo() {
     const inputElement = document.querySelector(".js-name-input")
@@ -9,4 +26,7 @@ function addTodo() {
     console.log(todoList);
 
     inputElement.value = "";
+
+    renderTodoList();
 }
+    
