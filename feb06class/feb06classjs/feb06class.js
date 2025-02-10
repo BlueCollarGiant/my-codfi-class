35e1212b4218c779dd9e81969a7f659efd0a13c2
BlueCollarGiant <167778887+BlueCollarGiant@users.noreply.github.com>
2025-02-10 17:31:37 -0600

    getting class files setup

---
+++ feb06class/feb06classjs/feb06class.js
@@ -0,0 +1,25 @@
+/*function renderMovies(listMovies) {
+    const movieHTML = listMovies.map((movie)=> `
+        <div class = "movie-container">
+            <div class = "movie-image-container">
+                <img class = "movie-image" src="${movie.image}" alt="${movie.title}">
+            </div>
+            <div class="movie-details">
+                <div class="movie-title limit-text-to-2-lines">
+                    ${movie.title}
+                </div>
+                <div class="movie-genre">
+                    ${movie.genre.join(',')}
+                </div>
+
+        </div>
+        
+    
+    
+    
+    
+    `)
+}
+    */
+
+
